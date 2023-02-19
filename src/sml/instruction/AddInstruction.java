8eeb8d4d4aeb55e44349560d20e7cd14a3c930a7
Szymon Eryk Swendrowski <szymon.swendrowski@gmail.com>
2023-02-19 17:06:17 +0000

    Wrote JavaDoc for AddInstruction class and its methods, haven't written or implemented methods equals() and hashcode() in that class yet.

--- src/sml/instruction/AddInstruction.java
+++ src/sml/instruction/AddInstruction.java
@@ -7,7 +7,9 @@ import sml.RegisterName;
 // TODO: write a JavaDoc for the class
 
 /**
- * @author
+ * Represents an add instruction.
+ *
+ * @author Szymon Swendrowski
  */
 
 public class AddInstruction extends Instruction {
@@ -16,12 +18,26 @@ public class AddInstruction extends Instruction {
 
 	public static final String OP_CODE = "add";
 
+	/**
+	 * Constructor: an instruction to add the values in two registers
+	 *
+	 * @param label optional label (can be null)
+	 * @param result the register to store the result in
+	 * @param source the register to add to the result
+	 */
 	public AddInstruction(String label, RegisterName result, RegisterName source) {
 		super(label, OP_CODE);
 		this.result = result;
 		this.source = source;
 	}
 
+	/**
+	 * Executes the add instruction in the given machine.
+	 *
+	 * @param m the machine the add instruction runs on
+	 * @return NORMAL_PROGRAM_COUNTER_UPDATE to indicate that
+	 *          the instruction with the next address is to be executed
+	 */
 	@Override
 	public int execute(Machine m) {
 		int value1 = m.getRegisters().get(result);
@@ -30,8 +46,23 @@ public class AddInstruction extends Instruction {
 		return NORMAL_PROGRAM_COUNTER_UPDATE;
 	}
 
+	/**
+	 * Computes the string representation of the add instruction.
+	 *
+	 * @return the string representation of the add instruction
+	 */
 	@Override
 	public String toString() {
 		return getLabelString() + getOpcode() + " " + result + " " + source;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return 0;
 	}
 }
