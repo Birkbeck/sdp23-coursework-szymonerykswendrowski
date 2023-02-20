9bff2727301703017746aa86c6f0fbccf8181b46
Szymon Eryk Swendrowski <szymon.swendrowski@gmail.com>
2023-02-20 16:15:42 +0000

    Implemented the divide instruction by creating the class DivInstruction, implemented it into the Translator getInstruction method.

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -85,6 +85,12 @@ public final class Translator {
                 return new MulInstruction(label, Register.valueOf(r), Register.valueOf(s));
             }
 
+            case DivInstruction.OP_CODE -> {
+                String r = scan();
+                String s = scan();
+                return new DivInstruction(label, Register.valueOf(r), Register.valueOf(s));
+            }
+
             // TODO: add code for all other types of instructions
 
             // TODO: Then, replace the switch by using the Reflection API
---
+++ src/sml/instruction/DivInstruction.java
@@ -0,0 +1,66 @@
+package sml.instruction;
+
+// TODO: write a JavaDoc for the class
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+/**
+ * Represents a divide instruction.
+ *
+ * @author Szymon Swendrowski
+ */
+public class DivInstruction extends Instruction {
+    public static final String OP_CODE = "div";
+    private final RegisterName result;
+    private final RegisterName source;
+
+    /**
+     * Constructor: an instruction to divide the values in two registers.
+     *
+     * @param label optional label (can be null)
+     * @param result the register to store the result in
+     * @param source the register to divide into the result
+     */
+    public DivInstruction(String label, RegisterName result, RegisterName source) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.source = source;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    /**
+     * Executes the divide instruction in the given machine.
+     *
+     * @param m the machine the divide instruction runs on
+     * @return NORMAL_PROGRAM_COUNTER_UPDATE to indicate that
+     *          the instruction with the next address is to be executed
+     */
+    @Override
+    public int execute(Machine m) {
+        int value1 = m.getRegisters().get(result);
+        int value2 = m.getRegisters().get(source);
+        m.getRegisters().set(result, value1 / value2);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    /**
+     * Computes the string representation of the divide instruction.
+     *
+     * @return the string representation of the divide instruction
+     */
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + result + " " + source;
+    }
+}
