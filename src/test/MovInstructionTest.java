780a8e4c29e5786a8debb83d6651c90435405629
Szymon Eryk Swendrowski <szymon.swendrowski@gmail.com>
2023-02-24 12:36:24 +0000

    Added test for the move instruction.

---
+++ src/test/MovInstructionTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+import sml.instruction.MovInstruction;
+
+import static sml.Registers.Register.EAX;
+
+public class MovInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void executeValid() {
+        registers.set(EAX, 0);
+        Instruction instruction = new MovInstruction(null, EAX, 5);
+        instruction.execute(machine);
+        Assertions.assertEquals(5, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidTwo() {
+        registers.set(EAX, -5);
+        Instruction instruction = new MovInstruction(null, EAX, -5);
+        instruction.execute(machine);
+        Assertions.assertEquals(-5, machine.getRegisters().get(EAX));
+    }
+}
