bc16409ded9ad4edfa4290fc4a3bd38e38968895
Szymon Eryk Swendrowski <szymon.swendrowski@gmail.com>
2023-02-24 11:01:00 +0000

    Added a test for the multiplication instruction.

---
+++ src/test/MulInstructionTest.java
@@ -0,0 +1,58 @@
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+import sml.instruction.MulInstruction;
+
+import static sml.Registers.Register.*;
+
+class MulInstructionTest {
+    private Machine machine;
+    private Registers registers;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        //...
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+        registers = null;
+    }
+
+    @Test
+    void executeValid() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new MulInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(30, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidTwo() {
+        registers.set(EAX, -5);
+        registers.set(EBX, 6);
+        Instruction instruction = new MulInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(-30, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void executeValidThree() {
+        registers.set(EAX, 0);
+        registers.set(EBX, 6);
+        Instruction instruction = new MulInstruction(null, EAX, EBX);
+        instruction.execute(machine);
+        Assertions.assertEquals(0, machine.getRegisters().get(EAX));
+    }
+}
