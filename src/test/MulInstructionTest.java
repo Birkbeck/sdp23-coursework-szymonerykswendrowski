aa72df5dcf63e7dc50b253f5e7369f174cac1bac
Szymon Eryk Swendrowski <szymon.swendrowski@gmail.com>
2023-02-27 21:53:32 +0000

    Implemented the JUnit test for the new methods added to the MulInstruction class in the previous commit, as well as renaming some of the old tests.

--- src/test/MulInstructionTest.java
+++ src/test/MulInstructionTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import sml.Instruction;
 import sml.Machine;
 import sml.Registers;
+import sml.instruction.AddInstruction;
 import sml.instruction.MulInstruction;
 
 import static sml.Registers.Register.*;
@@ -30,7 +31,7 @@ class MulInstructionTest {
     }
 
     @Test
-    void executeValid() {
+    void mulTestOne() {
         registers.set(EAX, 5);
         registers.set(EBX, 6);
         Instruction instruction = new MulInstruction(null, EAX, EBX);
@@ -39,7 +40,7 @@ class MulInstructionTest {
     }
 
     @Test
-    void executeValidTwo() {
+    void mulTestTwo() {
         registers.set(EAX, -5);
         registers.set(EBX, 6);
         Instruction instruction = new MulInstruction(null, EAX, EBX);
@@ -48,11 +49,47 @@ class MulInstructionTest {
     }
 
     @Test
-    void executeValidThree() {
+    void mulTestThree() {
         registers.set(EAX, 0);
         registers.set(EBX, 6);
         Instruction instruction = new MulInstruction(null, EAX, EBX);
         instruction.execute(machine);
         Assertions.assertEquals(0, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void equalsTestOne() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new MulInstruction(null, EAX, EBX);
+        Instruction instruction2 = new MulInstruction(null, EAX, EBX);
+        Assertions.assertTrue(instruction.equals(instruction2));
+    }
+
+    @Test
+    void equalsTestTwo() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        registers.set(ECX, 7);
+        Instruction instruction = new MulInstruction(null, EAX, EBX);
+        Instruction instruction2 = new MulInstruction(null, EAX, ECX);
+        Assertions.assertFalse(instruction.equals(instruction2));
+    }
+
+    @Test
+    void equalsMismatchTest() {
+        registers.set(EAX, 2);
+        registers.set(EBX, 3);
+        Instruction instruction = new MulInstruction(null, EAX, EBX);
+        Instruction instruction2 = new AddInstruction(null, EAX, EBX);
+        Assertions.assertFalse(instruction.equals(instruction2));
+    }
+
+    @Test
+    void hashCodeTest() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new MulInstruction(null, EAX, EBX);
+        Assertions.assertNotNull(instruction.hashCode());
     }
 }
