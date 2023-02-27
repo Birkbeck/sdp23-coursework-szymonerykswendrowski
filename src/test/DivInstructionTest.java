350d499488a0207a66d1251e7a35ce14231e24c9
Szymon Eryk Swendrowski <szymon.swendrowski@gmail.com>
2023-02-27 22:03:45 +0000

    Implemented equals() test for the DivInstruction class, also renamed the previous test to be clearer.

--- src/test/DivInstructionTest.java
+++ src/test/DivInstructionTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import sml.Instruction;
 import sml.Machine;
 import sml.Registers;
+import sml.instruction.AddInstruction;
 import sml.instruction.DivInstruction;
 
 import java.io.ByteArrayOutputStream;
@@ -38,7 +39,7 @@ class DivInstructionTest {
     }
 
     @Test
-    void executeValid() {
+    void zeroDivTest() {
         registers.set(EAX, 2);
         registers.set(EBX, 0);
         Instruction instruction = new DivInstruction(null, EAX, EBX);
@@ -49,7 +50,7 @@ class DivInstructionTest {
     }
 
     @Test
-    void executeValidTwo() {
+    void divTestOne() {
         registers.set(EAX, 4);
         registers.set(EBX, 2);
         Instruction instruction = new DivInstruction(null, EAX, EBX);
@@ -58,7 +59,7 @@ class DivInstructionTest {
     }
 
     @Test
-    void executeValidThree () {
+    void divTestTwo () {
         registers.set(EAX, 0);
         registers.set(EBX, 2);
         Instruction instruction = new DivInstruction(null, EAX, EBX);
@@ -67,7 +68,7 @@ class DivInstructionTest {
     }
 
     @Test
-    void executeValidFour () {
+    void negativeDivTest () {
         registers.set(EAX, 4);
         registers.set(EBX, -2);
         Instruction instruction = new DivInstruction(null, EAX, EBX);
@@ -76,11 +77,39 @@ class DivInstructionTest {
     }
 
     @Test
-    void executeValidFive () {
+    void remainderDivTest () {
         registers.set(EAX, 8);
         registers.set(EBX, 3);
         Instruction instruction = new DivInstruction(null, EAX, EBX);
         instruction.execute(machine);
         Assertions.assertEquals(2, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void equalsTestOne() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        Instruction instruction2 = new DivInstruction(null, EAX, EBX);
+        Assertions.assertTrue(instruction.equals(instruction2));
+    }
+
+    @Test
+    void equalsTestTwo() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        registers.set(ECX, 7);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        Instruction instruction2 = new DivInstruction(null, EAX, ECX);
+        Assertions.assertFalse(instruction.equals(instruction2));
+    }
+
+    @Test
+    void equalsMismatchTest() {
+        registers.set(EAX, 2);
+        registers.set(EBX, 3);
+        Instruction instruction = new DivInstruction(null, EAX, EBX);
+        Instruction instruction2 = new AddInstruction(null, EAX, EBX);
+        Assertions.assertFalse(instruction.equals(instruction2));
     }
 }
