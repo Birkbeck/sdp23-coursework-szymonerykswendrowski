ebe7d61ec5fd02778c7593267469e60c6276eefe
Szymon Eryk Swendrowski <szymon.swendrowski@gmail.com>
2023-02-27 21:37:08 +0000

    Added JUnit tests for the equals() and hashCode() methods for the SubInstruction class, as well as renaming the previous test to be more clear.

--- src/test/SubInstructionTest.java
+++ src/test/SubInstructionTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import sml.Instruction;
 import sml.Machine;
 import sml.Registers;
+import sml.instruction.AddInstruction;
 import sml.instruction.SubInstruction;
 
 import static sml.Registers.Register.*;
@@ -20,7 +21,6 @@ class SubInstructionTest {
     void setUp() {
         machine = new Machine(new Registers());
         registers = machine.getRegisters();
-        //...
     }
 
     @AfterEach
@@ -30,7 +30,7 @@ class SubInstructionTest {
     }
 
     @Test
-    void executeValid() {
+    void subTestOne() {
         registers.set(EAX, 5);
         registers.set(EBX, 6);
         Instruction instruction = new SubInstruction(null, EAX, EBX);
@@ -39,11 +39,47 @@ class SubInstructionTest {
     }
 
     @Test
-    void executeValidTwo() {
+    void subTestTwo() {
         registers.set(EAX, -5);
         registers.set(EBX, 6);
         Instruction instruction = new SubInstruction(null, EAX, EBX);
         instruction.execute(machine);
         Assertions.assertEquals(-11, machine.getRegisters().get(EAX));
+    }
+
+    @Test
+    void equalsTestOne() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new SubInstruction(null, EAX, EBX);
+        Instruction instruction2 = new SubInstruction(null, EAX, EBX);
+        Assertions.assertTrue(instruction.equals(instruction2));
+    }
+
+    @Test
+    void equalsTestTwo() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        registers.set(ECX, 7);
+        Instruction instruction = new SubInstruction(null, EAX, EBX);
+        Instruction instruction2 = new SubInstruction(null, EAX, ECX);
+        Assertions.assertFalse(instruction.equals(instruction2));
+    }
+
+    @Test
+    void equalsMismatchTest() {
+        registers.set(EAX, 2);
+        registers.set(EBX, 3);
+        Instruction instruction = new SubInstruction(null, EAX, EBX);
+        Instruction instruction2 = new AddInstruction(null, EAX, EBX);
+        Assertions.assertFalse(instruction.equals(instruction2));
+    }
+
+    @Test
+    void hashCodeTest() {
+        registers.set(EAX, 5);
+        registers.set(EBX, 6);
+        Instruction instruction = new AddInstruction(null, EAX, EBX);
+        Assertions.assertNotNull(instruction.hashCode());
     }
 }
